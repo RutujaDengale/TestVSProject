8a2531ceca647df43e1a42495f16157809605672
dev <dev@DESKTOP-4B3B8KD>
2019-06-13 09:52:16 +0530

    added contacy1

--- TestProject/TestProject/Controllers/HomeController.cs
+++ TestProject/TestProject/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace TestProject.Controllers
 
             return View();
         }
+
+        public ActionResult Contact1()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return null;
+        }
     }
 }
